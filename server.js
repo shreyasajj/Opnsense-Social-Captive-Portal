941a46bf99c4e847223e01898c495ccbb1588f1d
shreyasajj <shreyas.github@proton.me>
2025-12-13 01:41:34 -0600

    Added states

--- server.js
+++ server.js
@@ -394,7 +394,8 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL || 'http://localhost:3000/auth/google/callback'
+    callbackURL: process.env.CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
+    state: true,
   },
   (accessToken, refreshToken, profile, done) => {
     const user = {
